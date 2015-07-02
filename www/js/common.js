6a46b41f96dad57f15a8c5ff56a86bb7186cbc3b
kei4i <ilovegirlsinaglasses@gmail.com>
2015-07-02 10:56:41 +0300

    Initial commit

---
+++ www/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+});
+$(window).load(function() {
+
+        $(".").animated("fadeInDown", "fadeOut");
+        $(".left-sidebar").animated("flipInY", "fadeOut");
+        $(".section_1 nav").animated("fadeInRight", "fadeOutRight");
+        $(".section_3 ul li").animated("ZoomInRight", "fadeOut");
+        $(".header ul li a img").animated("fadeInUp", "fadeOut");
+        $("footer").animated("fadeIn", "fadeOut");
+
+
+    });
